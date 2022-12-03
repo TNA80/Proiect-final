dc2a0386ba71f556b441cfb220a696ff653a83a0
ceky <cekyy07@gmail.com>
2022-12-03 17:27:17 +0200

    Style programari

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,19 +13,16 @@ import { HomeComponent } from './components/home/home.component';
 import { AparateComponent } from './components/aparate/aparate.component';
 import { AboutComponent } from './components/about/about.component';
 import { ErrorComponent } from './components/error/error.component';
-import { ProgramariOnlineComponent } from './components/programari-online/programari-online.component';
-
-
 
 const appRoute: Routes = [
   // {path:'', component: HomeComponent},
-  {path:'', redirectTo:'Home', pathMatch: 'full'},
-  {path: 'Home', component: HomeComponent},
-  {path: 'Aparate', component: AparateComponent},
-  {path: 'About', component: AboutComponent},
-  {path: 'Programari', component: ProgramariComponent},
-  {path:'**',component: ErrorComponent}
-]
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+  { path: 'Aparate', component: AparateComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Programari', component: ProgramariComponent },
+  { path: '**', component: ErrorComponent },
+];
 
 @NgModule({
   declarations: [
@@ -37,17 +34,15 @@ const appRoute: Routes = [
     AparateComponent,
     AboutComponent,
     ErrorComponent,
-    ProgramariOnlineComponent
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     RouterModule.forRoot(appRoute),
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/programari/programari.component.ts
+++ src/app/components/programari/programari.component.ts
@@ -1,23 +1,53 @@
 // import { ThisReceiver } from '@angular/compiler';
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-programari',
   templateUrl: './programari.component.html',
-  styleUrls: ['./programari.component.css']
+  styleUrls: ['./programari.component.css'],
 })
+export class ProgramariComponent {
+  constructor(private http: HttpClient) {}
 
-export class ProgramariComponent  {
+  public onClickProgramareOnline(programari: {
+    First_name: string;
+    Last_name: string;
+    Email: string;
+    Aparate: string;
+    message: string;
+    data: string;
+  }) {
+    console.log(programari);
+    const headers = new HttpHeaders({ myHeader: 'Sklinique' });
+    this.http
+      .post<{ name: string }>(
+        'https://proiect-final-9a3c9-default-rtdb.europe-west1.firebasedatabase.app/programari.json',
+        programari,
+        { headers: headers }
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+
   public sendEmail(e: Event) {
     e.preventDefault();
-    emailjs.sendForm('service_4f30oql', 'template_7m6rzgt', e.target as HTMLFormElement, 'ah1vKo6QdFhoTaGa1')
-      .then((result: EmailJSResponseStatus) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-          
-      });
-        
+    emailjs
+      .sendForm(
+        'service_4f30oql',
+        'template_7m6rzgt',
+        e.target as HTMLFormElement,
+        'ah1vKo6QdFhoTaGa1'
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   }
 }
